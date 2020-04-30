5e7551679c8b05ebbde0d4f5c7554ed8b37b7134
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-04-30 14:40:16 +0930

    Adding questions

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,6 +71,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  questionNumber += 1;
                   scoreKeeper.add(Icon(
                     Icons.check,
                     color: Colors.green,
@@ -87,6 +95,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  questionNumber += 1;
                   scoreKeeper.add(
                     Icon(
                       Icons.close,
